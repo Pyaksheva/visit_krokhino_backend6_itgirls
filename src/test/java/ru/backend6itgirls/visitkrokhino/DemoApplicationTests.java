a81e71655cc865214a3dbc98dce9ae7a18b55569
elena <ele-emelina@yandex.ru>
2024-02-07 21:44:35 +0300

    first commit

---
+++ src/test/java/ru/backend6itgirls/visitkrokhino/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.backend6itgirls.visitkrokhino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
