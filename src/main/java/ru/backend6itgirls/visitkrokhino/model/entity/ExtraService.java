162b9f893441cf6763f4b271457ea012cd638b83
Sokolova_ES <shestak.ekaterina@gmail.com>
2024-02-18 14:25:08 +0300

    added fields and annotations for ExtraService entity class

--- src/main/java/ru/backend6itgirls/visitkrokhino/model/entity/ExtraService.java
+++ src/main/java/ru/backend6itgirls/visitkrokhino/model/entity/ExtraService.java
@@ -12,4 +12,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 public class ExtraService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int price;
 }
