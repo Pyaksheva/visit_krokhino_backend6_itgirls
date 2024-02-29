3bf4d6d70cb0c4bd745df7ccbc34996b7a271a7d
Sokolova_ES <shestak.ekaterina@gmail.com>
2024-02-29 20:12:21 +0300

    task4:fix repository classes

--- src/main/java/ru/backend6itgirls/visitkrokhino/repository/TimetableRepository.java
+++ src/main/java/ru/backend6itgirls/visitkrokhino/repository/TimetableRepository.java
@@ -2,11 +2,7 @@ package ru.backend6itgirls.visitkrokhino.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import ru.backend6itgirls.visitkrokhino.model.entity.Booking;
 import ru.backend6itgirls.visitkrokhino.model.entity.Timetable;
 
-import java.util.Optional;
-
 public interface TimetableRepository extends JpaRepository<Timetable, Long>, JpaSpecificationExecutor<Timetable> {
-    Optional<Timetable> findById(Long id);
 }
