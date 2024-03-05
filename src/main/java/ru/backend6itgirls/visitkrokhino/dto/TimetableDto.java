023cae5ec663b65e19876247a271fbc611a83dca
Sokolova_ES <shestak.ekaterina@gmail.com>
2024-03-05 20:18:23 +0300

    task22: add fields into TimetableDto

--- src/main/java/ru/backend6itgirls/visitkrokhino/dto/TimetableDto.java
+++ src/main/java/ru/backend6itgirls/visitkrokhino/dto/TimetableDto.java
@@ -1,4 +1,20 @@
 package ru.backend6itgirls.visitkrokhino.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class TimetableDto {
+    private int id;
+    private int mainCategoryId;
+    private LocalTime start;
+    private Duration duration;
 }
